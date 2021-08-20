ea2eb5208dc29eb1cdec8d525070cc4a9e9328df
keepithunnyt <keepithunnyt@gmail.com>
2021-08-20 15:53:25 +0900

    3rd

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
 import React from "react";
+import axios from "axios";
+import Movie from "./movie";
 
 class App extends React.Component {
   state = {
     isLoading: true,
     movies: [],
   };
+  getMovies = async () => {
+    // console.log(movies.data.data.movies);
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts.mx/api/v2/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+    //this.setState({ movies: movies });
+    // key movies : state / value movies : getmovies()
+  };
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000);
+    // axios.get 하는데 시간 걸림 - 그래서 함수 실행 후 그걸 바로 가져오게함
+    this.getMovies();
   }
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading
+          ? "Loading..."
+          : movies.map((movie) => {
+              console.log(movie);
+              return (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.poster}
+                />
+              );
+            })}
+      </div>
+    );
   }
 }
 
