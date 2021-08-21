ee5bcb9878c045cd3976d01dbd0eef3b4bfc12a0
keepithunnyt <keepithunnyt@gmail.com>
2021-08-21 22:24:16 +0900

    210821_reactjs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
-import Movie from "./movie";
+import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
   state = {
@@ -8,42 +9,41 @@ class App extends React.Component {
     movies: [],
   };
   getMovies = async () => {
-    // console.log(movies.data.data.movies);
     const {
       data: {
         data: { movies },
       },
     } = await axios.get(
-      "https://yts.mx/api/v2/list_movies.json?sort_by=rating"
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
     );
     this.setState({ movies, isLoading: false });
-    //this.setState({ movies: movies });
-    // key movies : state / value movies : getmovies()
   };
   componentDidMount() {
-    // axios.get 하는데 시간 걸림 - 그래서 함수 실행 후 그걸 바로 가져오게함
     this.getMovies();
   }
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <div>
-        {isLoading
-          ? "Loading..."
-          : movies.map((movie) => {
-              console.log(movie);
-              return (
-                <Movie
-                  key={movie.id}
-                  id={movie.id}
-                  year={movie.year}
-                  title={movie.title}
-                  summary={movie.summary}
-                  poster={movie.poster}
-                />
-              );
-            })}
-      </div>
+      <section class="container">
+        {isLoading ? (
+          <div class="loader">
+            <span class="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div class="movies">
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+            ))}
+          </div>
+        )}
+      </section>
     );
   }
 }
